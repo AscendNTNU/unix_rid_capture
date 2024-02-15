4ea892cd404cef17cdb02e298d88a3c971c884fb
andreasj0 <andreas.jonsebraten@gmail.com>
2024-02-15 11:54:54 +0100

    fixed correct network

---
+++ CMakeFiles/rid_capture.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/andreasj0/Desktop/remoteid_receiver/RemoteID_Receiver/unix_rid_capture/bluez_sniffer.c" "CMakeFiles/rid_capture.dir/bluez_sniffer.c.o" "gcc" "CMakeFiles/rid_capture.dir/bluez_sniffer.c.o.d"
+  "/home/andreasj0/Desktop/remoteid_receiver/RemoteID_Receiver/unix_rid_capture/display.c" "CMakeFiles/rid_capture.dir/display.c.o" "gcc" "CMakeFiles/rid_capture.dir/display.c.o.d"
+  "/home/andreasj0/Desktop/remoteid_receiver/RemoteID_Receiver/unix_rid_capture/export.c" "CMakeFiles/rid_capture.dir/export.c.o" "gcc" "CMakeFiles/rid_capture.dir/export.c.o.d"
+  "/home/andreasj0/Desktop/remoteid_receiver/RemoteID_Receiver/unix_rid_capture/france.c" "CMakeFiles/rid_capture.dir/france.c.o" "gcc" "CMakeFiles/rid_capture.dir/france.c.o.d"
+  "/home/andreasj0/Desktop/remoteid_receiver/RemoteID_Receiver/unix_rid_capture/m_per_deg.c" "CMakeFiles/rid_capture.dir/m_per_deg.c.o" "gcc" "CMakeFiles/rid_capture.dir/m_per_deg.c.o.d"
+  "/home/andreasj0/Desktop/remoteid_receiver/RemoteID_Receiver/unix_rid_capture/opendroneid.c" "CMakeFiles/rid_capture.dir/opendroneid.c.o" "gcc" "CMakeFiles/rid_capture.dir/opendroneid.c.o.d"
+  "/home/andreasj0/Desktop/remoteid_receiver/RemoteID_Receiver/unix_rid_capture/rid_capture.c" "CMakeFiles/rid_capture.dir/rid_capture.c.o" "gcc" "CMakeFiles/rid_capture.dir/rid_capture.c.o.d"
+  "/home/andreasj0/Desktop/remoteid_receiver/RemoteID_Receiver/unix_rid_capture/summary.c" "CMakeFiles/rid_capture.dir/summary.c.o" "gcc" "CMakeFiles/rid_capture.dir/summary.c.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
